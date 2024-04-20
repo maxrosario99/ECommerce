b5f02544efe0175680ef59e4f62439e27d8eb846
maxrosario99 <maxumum1000@gmail.com>
2024-04-20 12:04:14 -0400

    added cart function

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Container, Typography, Button, Grid} from "@material-ui/core"
 
 import useStyles from "./styles"
+import Cartitem from './Cartitem/Cartitem'
 const Cart = ({cart}) => {
 
 
@@ -15,8 +16,7 @@ const Cart = ({cart}) => {
             <Grid container spacing={3} >
                 {cart.line_items.map((item) => 
                 <Grid item xs={12} sm={4} key={item.id}>
-                    <div>{item.name}</div>
-
+                    <Cartitem item={item}/>
                 </Grid>
                 
                 )}
@@ -35,13 +35,13 @@ const Cart = ({cart}) => {
             </div>
         </>
     )
-  return import(
+    if (!cart.line_items) return "loading..."
+  return (
     <Container>
         <div className={classes.toolbar}  />
-        <Typography className= {classes.title} variant="h3">Your Shopping Cart</Typography>
+        <Typography className= {classes.title} variant="h3" gutterBottom>Your Shopping Cart</Typography>
     </Container>
 
     )
 }
-import {Container, Typography, Button, Grid} from "@material-ui/core"
 export default Cart
